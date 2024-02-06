0d939db7389f7a6d82bf348e22460dd05b2be5a9
utk145 <utkarsh20241a0514@grietcollege.com>
2024-02-06 09:41:20 +0530

    file-rename refractor

--- app/checkout/_layout.tsx
+++ app/checkout/_layout.tsx
@@ -19,7 +19,7 @@ export default function CheckoutStack() {
                 },
                 headerTintColor: "#f0ebf8"
             }}>
-                <Stack.Screen name="index" options={{ title: "Personal Information", headerTitleAlign: "center" }} />
+                <Stack.Screen name="personal" options={{ title: "Personal Information", headerTitleAlign: "center" }} />
                 <Stack.Screen name="delivery" options={{ title: "Delivery Information", headerTitleAlign: "center" }} />
                 <Stack.Screen name="payment" options={{ title: "Payment Information", headerTitleAlign: "center" }} />
             </Stack>
