beb685eb5d5d5a8fb04f531cf25197ce539a09fa
utk145 <utkarsh20241a0514@grietcollege.com>
2024-02-09 08:39:01 +0530

    .

--- src/schema/checkout.schema.ts
+++ src/schema/checkout.schema.ts
@@ -1,21 +1,43 @@
-// https://zod.dev/?id=basic-usage
-import { z as zodInstance } from "zod"; // just for better naming 
-/* If you mean to say "Is z a Zod instance?" in the context of a specific piece of code you're looking at, it's important to understand that z itself is not an instance; it's the module or namespace that provides methods for creating instances of Zod schemas. */
+// // https://zod.dev/?id=basic-usage
+// import { z as zodInstance } from "zod"; // just for better naming 
+// /* If you mean to say "Is z a Zod instance?" in the context of a specific piece of code you're looking at, it's important to understand that z itself is not an instance; it's the module or namespace that provides methods for creating instances of Zod schemas. */
 
-// creating an object schema
-const personalInfo = zodInstance.object({
-    name: zodInstance.string(),
-    email: zodInstance.string().email(),
+// // creating an object schema
+// const personalInfo = zodInstance.object({
+//     name: zodInstance.string(),
+//     // email: zodInstance.string().email(),
+//     email: zodInstance.string().email({message:"Please enter a valid email address"}),
+// })
+
+
+// // parsing
+// const info = personalInfo.parse(
+//     {
+//         name: "Luffy Shera",
+//         email: "dotyh@alo.com"
+//     }
+// )
+
+// console.log(info);
+
+
+
+// Method 2:-
+import { z as zodInstance } from "zod";
+
+
+const PersonalInfoSchema = zodInstance.object({
+    name: zodInstance.string().min(5),
+    email: zodInstance.string().email({ message: "Please enter a valid email address" }),
 })
 
+type PersonalInfo = zodInstance.infer<typeof PersonalInfoSchema>
 
-// parsing
-const info = personalInfo.parse(
+const info: PersonalInfo = PersonalInfoSchema.parse(
     {
-        name: "Luffy Shera",
+        name: "Lu",
         email: "dotyh@alo.com"
     }
 )
 
 console.log(info);
-
